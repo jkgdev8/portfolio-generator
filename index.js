c93ea76ca2bc42eae9203bcb314b8446452015e7
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2021-12-10 22:48:44 -0800

    generated html file complete

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const commandLineArgs = process.argv;
-console.log(profileDataArgs);
+//9.2.6
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
+//fs.writeFile(file, data[, options], callback)
+//^^first, second, third arguments^^
 
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js');
 
 
-const message = 'Hello Node!';
+const fs = require('fs');
 
-if (true === true) {
-  const message = 'Hello ES6!';
-  let sum = 5;
-  sum += 10;
-  console.log(message);
-  console.log(sum);
-}
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
 
