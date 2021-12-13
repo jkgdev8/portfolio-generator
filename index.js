fee49d991b76d9bda72e6fcdbd87fb59771edcce
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2021-12-13 14:08:47 -0800

    progress for feature questions

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-//9.2.6
+//9.3.5
 
 //fs.writeFile(file, data[, options], callback)
 //^^first, second, third arguments^^
 
-const generatePage = require('./src/page-template.js');
+
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+ 
 
 
-const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
